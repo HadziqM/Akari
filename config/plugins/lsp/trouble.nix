38201bd7aa964fdb92a0bd4f7394d5518da91897
HadziqM <dimascrazz@gmail.com>
2025-02-24 15:43:03 +0700

    feat: fix trouble plugins

--- config/plugins/lsp/trouble.nix
+++ config/plugins/lsp/trouble.nix
@@ -7,44 +7,80 @@
   keymaps = [
     {
       mode = "n";
-      key = "<leader>x";
-      action = "+diagnostics/quickfix";
-    }
-    {
-      mode = "n";
       key = "<leader>xx";
-      action = "<cmd>TroubleToggle<cr>";
-      options = {
-        silent = true;
-        desc = "Document Diagnostics (Trouble)";
-      };
+      action = "<cmd>Trouble diagnostics toggle<cr>";
+      options.desc = "Diagnostics (Trouble)";
     }
     {
       mode = "n";
       key = "<leader>xX";
-      action = "<cmd>TroubleToggle workspace_diagnostics<cr>";
-      options = {
-        silent = true;
-        desc = "Workspace Diagnostics (Trouble)";
-      };
+      action = "<cmd>Trouble diagnostics toggle filter.buf=0<cr>";
+      options.desc = "Buffer Diagnostics (Trouble)";
     }
     {
       mode = "n";
-      key = "<leader>xt";
-      action = "<cmd>TroubleToggle todo<cr>";
-      options = {
-        silent = true;
-        desc = "Todo (Trouble)";
-      };
+      key = "<leader>cs";
+      action = "<cmd>Trouble symbols toggle focus=false<cr>";
+      options.desc = "Symbols (Trouble)";
     }
     {
       mode = "n";
-      key = "<leader>xq";
-      action = "<cmd>TodoQuickFix<cr>";
-      options = {
-        silent = true;
-        desc = "Quickfix List (Trouble)";
-      };
+      key = "<leader>cl";
+      action = "<cmd>Trouble lsp toggle focus=false win.position=right<cr>";
+      options.desc = "LSP Definitions / references / ... (Trouble)";
     }
+    {
+      mode = "n";
+      key = "<leader>xL";
+      action = "<cmd>Trouble loclist toggle<cr>";
+      options.desc = "Location List (Trouble)";
+    }
+    {
+      mode = "n";
+      key = "<leader>xQ";
+      action = "<cmd>Trouble qflist toggle<cr>";
+      options.desc = "Quickfix List (Trouble)";
+    }
+    {
+      mode = "n";
+      key = "<leader>x";
+      action = "+diagnostics/quickfix";
+    }
+    # {
+    #   mode = "n";
+    #   key = "<leader>xx";
+    #   action = "<cmd>TroubleToggle<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Document Diagnostics (Trouble)";
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<leader>xX";
+    #   action = "<cmd>TroubleToggle workspace_diagnostics<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Workspace Diagnostics (Trouble)";
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<leader>xt";
+    #   action = "<cmd>TroubleToggle todo<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Todo (Trouble)";
+    #   };
+    # }
+    # {
+    #   mode = "n";
+    #   key = "<leader>xq";
+    #   action = "<cmd>TodoQuickFix<cr>";
+    #   options = {
+    #     silent = true;
+    #     desc = "Quickfix List (Trouble)";
+    #   };
+    # }
   ];
 }
